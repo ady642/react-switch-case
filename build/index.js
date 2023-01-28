596de2767dad85e062f6ad98803f565bf170ccea
Adrien Haik <ady642@gmail.com>
2023-01-28 12:17:20 +0100

    feat: remove type:module

--- build/index.js
+++ build/index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-Object.defineProperty(exports, '__esModule', { value: true });
-
 var Case = function (props) {
     return props.children;
 };
--- src/__tests__/component.spec.tsx
+++ src/__tests__/component.spec.tsx
@@ -1,13 +1,11 @@
 import * as React from "react";
 import { Switch, Case } from "..";
-import * as Adapter from 'enzyme-adapter-react-16';
-import {configure, mount} from 'enzyme';
-
-configure({adapter: new Adapter()});
+import {render} from "@testing-library/react";
+import { describe, it, expect } from "vitest";
 
 let wrapper = null
 
-const createWrapper = (componentName = '') => mount(<Switch componentName={componentName} defaultComponent={<div>Default component</div>}>
+const createWrapper = (componentName = '') => render(<Switch componentName={componentName} defaultComponent={<div>Default component</div>}>
     <Case value={'HelloComponent'}><div>Hello</div></Case>
     <Case value={'ByeComponent'}><div>Bye</div></Case>
 </Switch>);
@@ -16,16 +14,16 @@ describe("Component should show 'red' text 'Hello World'", () => {
     it('Switch should render Hello if HelloComponent is the componentName', () => {
         wrapper = createWrapper('HelloComponent')
 
-        expect(wrapper.text()).toEqual('Hello')
+        expect(wrapper.getAllByText('Hello')).toBeDefined()
     })
     it('Switch should render Bye if ByeComponent is the componentName', () => {
         wrapper = createWrapper('ByeComponent')
 
-        expect(wrapper.text()).toEqual('Bye')
+        expect(wrapper.getAllByText('Bye')).toBeDefined()
     })
     it('Switch should render the defaultComponent if the componentName does not match any Case', () => {
         wrapper = createWrapper('OtherComponent')
 
-        expect(wrapper.text()).toEqual('Default component')
+        expect(wrapper.getAllByText('Default component')).toBeDefined()
     })
 });
